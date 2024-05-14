c897f64d56fd2bbd734fb2dd4d9d6f110b4165f4
Iron <ironrobeygo@gmail.com>
2024-05-14 14:05:01 +0800

    feat: updated QuoteController to accept post

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/get-quote-as-pdf', [QuoteController::class, 'index']);
+Route::post('/get-quote-as-pdf', [QuoteController::class, 'index']);
