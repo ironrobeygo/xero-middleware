16b033380e7c104ca2e360499977f74579aac3ef
Iron <ironrobeygo@gmail.com>
2024-05-30 22:05:07 +0800

    feat added sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuoteController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/get-quote-as-pdf', [QuoteController::class, 'index']);
+Route::post('/get-quote-as-pdf', [QuoteController::class, 'index'])->middleware('auth:sanctum');
